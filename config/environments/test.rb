51a19f2f96f8c616a715ed9ac60960bb4e976e59
Neftaly Luis <neftaly.luis@resuelve.mx>
2019-07-25 19:47:28 -0500

    Rubocop

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -43,4 +45,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.raise = true # raise an error if n+1 query occurs
+  end
 end
